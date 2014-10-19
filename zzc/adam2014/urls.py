c647fe260a78fa8b2382c538456038ff74f25097
Lao Frank <rogggg@126.com>
2014-10-19 16:49:59 +0800

    add a helloworld demo

---
+++ zzc/adam2014/urls.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# URLconf
+from django.conf.urls import patterns ,url ,include
+
+from demo import views
+
+urlpatterns = patterns("", 
+ 	url(r'^hello/$',views.helloworld,name='helloworld'),
+ 	# url(r'^ueditor/',include('DjangoUeditor.urls' )),
+ 	# url(r'^ueditor/$',views.get_editor,name="geteditor"),
+	)
+
+
--- zzc/zzc/urls.py
+++ zzc/zzc/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^demo/',include('demo.urls',namespace="demo")),
     # url(r'^static/(?P<path>.*)$', 'django.views.static.serve'),
 ) 
 
